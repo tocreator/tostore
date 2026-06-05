b0ae3a27147b804049981b921907d96b266a0ec2
tocreator <9876783@qq.com>
2026-06-06 06:17:40 +0800

    feat: refine public database API

--- lib/src/Interface/chain_builder.dart
+++ lib/src/Interface/chain_builder.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import '../core/data_store_impl.dart';
 import '../core/workload_scheduler.dart';
+import '../model/db_exception.dart';
+import '../model/result_type.dart';
 import '../handler/logger.dart';
 import '../model/table_schema.dart';
 import '../query/query_condition.dart';
